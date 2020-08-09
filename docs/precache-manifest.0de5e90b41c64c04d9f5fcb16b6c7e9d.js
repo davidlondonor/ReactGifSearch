4e44f9bfb3c610d36e5a72a14a6aae54e5eba219
davidlucky@gmail.com <davidlucky@gmail.com>
2020-08-09 17:40:51 -0500

    first commit

---
+++ docs/precache-manifest.0de5e90b41c64c04d9f5fcb16b6c7e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109a202a545b397be9be39ec1c562b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee000d2902e7e4fe13ce",
+    "url": "/static/css/main.4cdd04f2.chunk.css"
+  },
+  {
+    "revision": "01ca35a3cfa04a8afa27",
+    "url": "/static/js/2.c353c2d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c353c2d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee000d2902e7e4fe13ce",
+    "url": "/static/js/main.4be86353.chunk.js"
+  },
+  {
+    "revision": "63ef23f6ed094e308245",
+    "url": "/static/js/runtime-main.485fa638.js"
+  }
+]);
